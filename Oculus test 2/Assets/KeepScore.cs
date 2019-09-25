5644260a2ae0e215a2ef1f5854a126f76fa9f6d7
borabayk <50092501+borabayk@users.noreply.github.com>
2019-09-25 14:40:44 +1000

    Initial commit

---
+++ Oculus test 2/Assets/KeepScore.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeepScore : MonoBehaviour
+{
+    public int RedPlayerScore;
+    public int BluePlayerScore;
+
+    public Text RedPlayerScoreText;
+    public Text BluePlayerScoreText;
+    public Text GameState;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "RedPointWin")
+        {
+            RedPlayerAddScore();
+
+            //GameState.text = "Game Over!";
+            //Time.timeScale = 0;
+        }
+
+        if (other.tag == "BluePointWin")
+        {
+            BluePlayerAddScore();
+        }
+
+    }
+
+    
+
+    void BluePlayerAddScore()
+    {
+        BluePlayerScore++;
+        BluePlayerScoreText.text = BluePlayerScore.ToString();
+    }
+
+
+    void RedPlayerAddScore()
+    {
+        RedPlayerScore++;
+        RedPlayerScoreText.text = RedPlayerScore.ToString();
+    }
+
+
+}
---
+++ Oculus test 2/Assets/PlayerBorder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBorder : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        Ball ball = col.gameObject.GetComponent<Ball>();
+        if(ball != null)
+        {
+            ball.transform.position = new Vector3(0f, 1f, 0f);
+        }
+    }
+
+}
